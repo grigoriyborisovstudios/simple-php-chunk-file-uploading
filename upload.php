9d5d8cd19cb44e4ff3408159f621362c1f4feb90
d3se <135171210+d3se@users.noreply.github.com>
2023-07-21 17:45:19 +0200

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,47 +1,27 @@
 <?php
-// Define a constant for the upload directory (Визначаємо константу для директорії завантаження)
-define('UPLOAD_DIR', 'uploads/');
+$fileName = $_FILES['file']['name']; // Get uploaded file name | Отримуємо ім'я завантаженого файлу
+$chunkIndex = $_POST['chunkIndex']; // Get current chunk index | Отримуємо індекс поточної частини
+$totalChunks = $_POST['totalChunks']; // Get total number of chunks | Отримуємо загальну кількість частин
 
-// Check if the $_FILES['file'] array is not empty (Перевіряємо, чи не пустий масив $_FILES['file'])
-if (!empty($_FILES['file'])) {
-    // Get the original file name (Отримуємо оригінальне ім'я файлу)
-    $fileName = $_POST['filename'];
-    // Define the target directory (Визначаємо цільову директорію)
-    $targetDir = UPLOAD_DIR . $fileName;
+$chunkFile = $fileName . '.part' . $chunkIndex; // Create chunk file name | Створюємо ім'я файлу частини
+move_uploaded_file($_FILES['file']['tmp_name'], 'uploads/' . $chunkFile); // Move chunk file to uploads folder | Переміщуємо файл частини до теки uploads
 
-    // Open the file for writing (Відкриваємо файл для запису)
-    $out = fopen($targetDir, "ab");
-    // If the file is opened successfully (Якщо файл вдало відкрито)
-    if ($out) {
-        // Open the temporary file for reading (Відкриваємо тимчасовий файл для читання)
-        $in = fopen($_FILES['file']['tmp_name'], "rb");
-        // If the temporary file is opened successfully (Якщо тимчасовий файл вдало відкрито)
-        if ($in) {
-            // Read and write file chunks (Читаємо і записуємо фрагменти файлу)
-            while ($chunk = fread($in, 4096)) {
-                fwrite($out, $chunk, 4096);
-            }
+// If this was the last chunk
+// Якщо це була остання частина
+if ($chunkIndex == $totalChunks - 1) {
+    // Reassemble all chunks
+    // Збираємо всі частини в один файл
+    $outFile = fopen('uploads/' . $fileName, 'wb'); // Open output file | Відкриваємо вихідний файл
+    for ($i = 0; $i < $totalChunks; $i++) {
+        $inFile = fopen('uploads/' . $fileName . '.part' . $i, 'rb'); // Open input chunk file | Відкриваємо вхідний файл частини
+        while ($chunk = fread($inFile, 8192)) {
+            fwrite($outFile, $chunk); // Write chunk to output file | Записуємо частину до вихідного файлу
         }
-        // Close the files (Закриваємо файли)
-        fclose($in);
-        fclose($out);
-
-        // Delete the temporary file (Видаляємо тимчасовий файл)
-        unlink($_FILES['file']['tmp_name']);
+        fclose($inFile); // Close input chunk file | Закриваємо вхідний файл частини
+        unlink('uploads/' . $fileName . '.part' . $i); // Delete input chunk file | Видаляємо вхідний файл частини
     }
-
-    // Send a response with a successful status (Надсилаємо відповідь із успішним статусом)
-    echo json_encode([
-        'status' => true,
-        'message' => 'File uploaded successfully',
-        'messageua' => 'Файл успішно завантажено'
-        
-    ]);
-} else {
-    // Send a response with a failed status (Надсилаємо відповідь із невдалим статусом)
-    echo json_encode([
-        'status' => false,
-        'message' => 'No file uploaded'
-    ]);
+    fclose($outFile); // Close output file | Закриваємо вихідний файл
 }
+
+echo 'OK'; // Send response | Надсилаємо відповідь
 ?>
